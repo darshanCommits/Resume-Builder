2c5d3210926c9eeae76d2bca07d24b2147c9032d
Darshan Kumawat <kumawatdarshan.1304@gmail.com>
2024-02-20 19:07:20 +0530

    refactor: ResumeSchema: declared-> derived

--- src/models/resumeTypes.ts
+++ src/models/resumeTypes.ts
@@ -1,48 +1,22 @@
-type Website = {
-	website?: string;
-	summary?: string;
+import {
+	about,
+	education,
+	experiences,
+	projects,
+	skills,
+	social,
+} from "@data/exampleResume";
+
+export type InferFromRecord<T extends Record<string, unknown>> = {
+	[K in keyof T]: T[K] extends (infer U)[] ? U[] : T[K];
 };
 
-type Date = {
-	date_or_date_range: string;
-};
-
-export type AboutSchema = Website & {
-	full_name: string;
-	headline: string;
-	email: string;
-	phone: string;
-	location: string;
-};
-
-export type SocialSchema = Website & {
-	network: string;
-	username: string;
-};
-
-export type ExperienceSchema = Website &
-	Date & {
-		company: string;
-		position: string;
-		location: string;
-	};
-
-export type EducationSchema = Website &
-	Date & {
-		institution: string;
-		type_of_study: string;
-		area_of_study: string;
-		score: string;
-	};
-
-export type ProjectSchema = Date &
-	Website & {
-		name: string;
-		keywords: string[];
-		summary: string;
-	};
-
-export type SkillSchema = string[];
+export type AboutSchema = InferFromRecord<typeof about>;
+export type SocialSchema = InferFromRecord<typeof social[number]>;
+export type ExperienceSchema = InferFromRecord<typeof experiences[number]>;
+export type EducationSchema = InferFromRecord<typeof education[number]>;
+export type ProjectSchema = InferFromRecord<typeof projects[number]>;
+export type SkillSchema = typeof skills;
 
 export type ResumeSchema = {
 	about: AboutSchema;
@@ -54,17 +28,14 @@ export type ResumeSchema = {
 };
 
 export type SingleSectionTypes = {
-	about: AboutSchema;
-	social: SocialSchema;
-	experiences: ExperienceSchema;
-	education: EducationSchema;
-	projects: ProjectSchema;
-	skills: SkillSchema;
+	[K in keyof ResumeSchema]: ResumeSchema[K] extends (infer U)[]
+		? U
+		: ResumeSchema[K];
 };
 
-export type SectionKeys = keyof SingleSectionTypes;
+export type SectionKeys = keyof ResumeSchema;
 
-export type SingleSectionSchema<T extends SectionKeys> = SingleSectionTypes[T];
+export type SectionSchema<T extends SectionKeys> = ResumeSchema[T];
 
 export type RefObjectMap<T> = {
 	[key: string]: React.RefObject<T>;
